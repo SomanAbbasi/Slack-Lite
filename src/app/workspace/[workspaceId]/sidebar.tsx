c0b7561f57d4cfdc77bdbed099c8faf7662db0da
Agentux <contact.agentux@gmail.com>
2026-05-04 23:31:32 +0500

    Sidebar icon completion

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -2,17 +2,25 @@
 import { UserButton } from "@/features/auth/components/user-button"
 import { WorkSpaceSwitcher } from "./workspace-switcher"
 import { SidebarButton } from "./sidebar-button"
-import { Home } from "lucide-react"
+import { BellIcon, Home, MessagesSquare, MoreHorizontal } from "lucide-react"
+import { usePathname } from "next/navigation"
 
 export const Sidebar=()=>{
+
+    const pathname=usePathname();
+
 
     return (
         <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[0px] pb-4px">
 
                 <WorkSpaceSwitcher/>
 
+{/* ={pathname.includes("/workspace")} */}
                 <SidebarButton icon={Home} label="Home" isActive/>
 
+                <SidebarButton icon={MessagesSquare} label="DMs" />
+                <SidebarButton icon={BellIcon} label="Activity" />
+                <SidebarButton icon={MoreHorizontal} label="More" />
 
             <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
                 <UserButton/>
